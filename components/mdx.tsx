294a224051f3c69edacbfe630f11a3ebd752c6eb
Vibhu Agarwal <16359904+vibhuagwl@users.noreply.github.com>
2026-08-10 01:52:58 +0530

    Connect Mermaid to MDX articles

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,3 +1,2 @@
-import {ReactNode} from 'react';
-export const mdxComponents={h2:(p:any)=><h2 {...p}/>,h3:(p:any)=><h3 {...p}/>,p:(p:any)=><p {...p}/>,ul:(p:any)=><ul {...p}/>,ol:(p:any)=><ol {...p}/>,pre:(p:any)=><pre {...p}/>,blockquote:(p:any)=><blockquote {...p}/>,code:(p:any)=><code {...p}/>};
-export function Mermaid({children}:{children:ReactNode}){return <pre className="not-prose"><code>{children}</code></pre>}
+import Mermaid from './mermaid';
+export const mdxComponents={h2:(p:any)=><h2 {...p}/>,h3:(p:any)=><h3 {...p}/>,p:(p:any)=><p {...p}/>,ul:(p:any)=><ul {...p}/>,ol:(p:any)=><ol {...p}/>,pre:(p:any)=><pre {...p}/>,blockquote:(p:any)=><blockquote {...p}/>,code:(p:any)=><code {...p}/>,Mermaid};
