2271dad47529bd95740a7c402398010644de596b
Vibhu Agarwal <16359904+vibhuagwl@users.noreply.github.com>
2026-08-10 01:54:33 +0530

    Configure class-based dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,2 +1,2 @@
 import type { Config } from 'tailwindcss';
-export default { content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'], theme: { extend: {} }, plugins: [] } satisfies Config;
+export default { darkMode:'class', content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'], theme: { extend: {} }, plugins: [] } satisfies Config;
